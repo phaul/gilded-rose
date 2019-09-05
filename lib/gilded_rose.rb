76d44ad3587dd432a4e6a627081053ab73ed184e
Paul Sonkoly <sonkoly.pal@gmail.com>
2019-09-05 13:30:36 +0100

    Rubocop, reek, etc fixes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,22 +1,25 @@
 require 'forwardable'
 
 class GildedRose
-
   def initialize(items)
     @items = items
   end
 
   def update_quality()
-    @items.each do |item|
-      item.update
-    end
+    @items.each(&:update)
   end
 end
 
+# Item quality
+#
+# A type safety wrapper on integers, so arbitrary arithmetics don't work.
+# Quality changes values according to strict rules.
 class Quality
   extend Forwardable
   include Comparable
 
+  # New quality
+  # @param quality [Integer] The non-wrapped quality value
   def initialize(quality)
     @quality = quality
   end
@@ -25,61 +28,79 @@ class Quality
   def_delegator :@quality, :to_s
   def_delegator :@quality, :<=>
 
+  # Increases the quality by one
   def increase
     @quality += 1 if @quality < 50
   end
 
+  # Decreases the quality by one
   def decrease
-    @quality -= 1 if @quality > 0
+    @quality -= 1 if @quality.positive?
   end
 
+  # Resets quality to 0
   def reset
     @quality = 0
   end
 end
 
+# Item has a name, a quality and a sell_in
 class Item
   attr_accessor :name, :sell_in
 
+  # @return [Integer] quality value
   def quality
     @quality.to_i
   end
 
-  def quality=(i)
-    @quality = Quality.new(i)
+  # Sets the quality value to i
+  # @param quality [Integer] quality
+  def quality=(quality)
+    @quality = Quality.new(quality)
   end
 
+  # new item
+  # @param name [String] Item name
+  # @param sell_in [Integer] sell in quantities
+  # @param quality [Integer] item quality
   def initialize(name, sell_in, quality)
     @name = name
     @sell_in = sell_in
     @quality = Quality.new(quality)
   end
 
-  def to_s()
+  # @see Object.to_s
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 
-  def self.for(name: , sell_in: , quality: )
-    if name == 'Aged Brie' then
-      AgedBrie.new(name, sell_in, quality)
-    elsif name == 'Backstage passes to a TAFKAL80ETC concert'
+  # Item factory. Produces an item with the appropriate type
+  # @param name [String] Item name
+  # @param sell_in [Integer] sell in quantities
+  # @param quality [Integer] item quality
+  def self.for(name:, sell_in:, quality:)
+    case name
+    when 'Aged Brie' then AgedBrie.new(name, sell_in, quality)
+    when 'Backstage passes to a TAFKAL80ETC concert'
       Backstage.new(name, sell_in, quality)
-    elsif name == 'Sulfuras, Hand of Ragnaros'
-      Sulfuras.new(name, sell_in, quality)
-    else
-      new(name, sell_in, quaility)
+    when 'Sulfuras, Hand of Ragnaros' then Sulfuras.new(name, sell_in, quality)
+    else new(name, sell_in, quaility)
     end
   end
 
+  # Item with name being Aged Brie
   class AgedBrie < Item
+    # @see {Item.update}
     def update
       @quality.increase
       @sell_in -= 1
-      @quality.increase if sell_in < 0
+      @quality.increase if sell_in.negative?
     end
   end
 
+  # Item with name being Backstage passes to a TAFKAL80ETC concert
   class Backstage < Item
+    # @see {Item.update}
     def update
       @quality.increase
       @quality.increase if @sell_in < 11
@@ -89,14 +110,16 @@ class Item
     end
   end
 
+  # Item with name being Sulfuras, Hand of Ragnaros
   class Sulfuras < Item
-    def update
-    end
+    # @see {Item.update}
+    def update; end
   end
 
+  # Updates item quality and sell_in according to Gilded Rose rules
   def update
     @quality.decrease
     @sell_in -= 1
-    @quality.decrease if @sell_in < 0
+    @quality.decrease if @sell_in.negative?
   end
 end
